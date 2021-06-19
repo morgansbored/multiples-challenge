f76cd9299686fb9d2b8ee46875c1999a7f60eb09
“morganpearse” <morgan@morganpearse.com>
2021-06-19 11:31:08 +0100

    multiples function definition

--- src/multiples.js
+++ src/multiples.js
@@ -2,27 +2,42 @@ const output = document.querySelector('.output');
 const userInput = document.getElementById('input-int');
 
 function getMultiples(inInt) {
-  //...
+  if (inInt < 0) return 0;
+
+  let result = {
+    sum: 0,
+    multiples: [] //just for show
+  };
+
+  for (let i = 0; i < inInt; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      if (i > 0) result.multiples.push(i);
+      result.sum += i;
+    }
+  }
+
   return result;
 }
 
 function showResults() {
   const result = getMultiples(userInput.value);
+
   output.innerHTML = 
     `<p>You input the number ${userInput.value}</p>`;
-  if (result === 0) {
+    if (result === 0) {
     output.innerHTML += 
     `<p>The result is: ${result}</p>
     `;
   } else {
     output.innerHTML += 
-    `<p>Multiples of 3 or 5 below ${userInput.value} are:</p>
-    <p>They add up to: ${result}</p>
+    `<p>Multiples of 3 or 5 below ${userInput.value} are: ${result.multiples}</p>
+    <p>They add up to: ${result.sum}</p>
     `;
   }
 }
 
 function startOver() {
+  //clear input box and displayed output
   userInput.value = '';
   output.innerHTML = '';
 }
